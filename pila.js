94aa8ed1ea7c8abf62593c00b2b1696127b5dfe0
YWCDiegosieraa <diego.sierra02@unicatolica.edu.co>
2026-04-17 14:19:41 -0500

    Se agrega logica de pila

---
+++ pila.js
@@ -0,0 +1,32 @@
+class Pila {
+    constructor() {
+        this.items = [];
+    }
+    // Agregar
+    agregar(elemento) {
+        this.items.push(elemento);
+        console.log("Se agregó:", elemento);
+    }
+    // Eliminar
+    eliminar() {
+        if (this.items.length === 0) {
+            console.log("La pila está vacía");
+            return;
+        }
+        let eliminado = this.items.pop();
+        console.log("Se eliminó:", eliminado);
+    }
+    mostrar() {
+        console.log("Pila actual:", this.items);
+    }
+}
+
+let pila = new Pila();
+
+pila.agregar({ nombre: "Juan", edad: 24 });
+pila.agregar({ nombre: "Claudia", edad: 21 });
+
+pila.mostrar();
+
+pila.eliminar();
+pila.mostrar();
